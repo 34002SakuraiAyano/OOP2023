2c70b09b7de5b8f2e006345e34d84f69e70cf954
櫻井彩乃 <OIBC34002@ojs.ac.jp>
2023-04-25 11:18:49 +0900

    右クリックでテニスボール

--- Sample/BallApp/Program.cs
+++ Sample/BallApp/Program.cs
@@ -9,53 +9,67 @@ using System.Windows.Forms;
 namespace BallApp {
     class Program : Form {
 
-        private Timer moveTimer; //タイマー用
+        private Timer moveTimer;    //タイマー用
         private SoccerBall soccerBall;
-        private PictureBox pb ; //画像を表示するコントロール
+        private TennisBall tennisBall;
+        private PictureBox pb;
 
-        private List<SoccerBall> balls = new List<SoccerBall>(); //ボールインスタンス格納用
-        private List<PictureBox> pbs = new List<PictureBox>();   // 表示用
-
+        //ObjにSoccerBallとTennisBallが入ってる
+        private List<Obj> balls = new List<Obj>();    //ボールインスタンス格納用
+        private List<PictureBox> pbs = new List<PictureBox>();      //表示用
 
         static void Main(string[] args) {
             Application.Run(new Program());
         }
 
         public Program() {
-          
             this.Size = new Size(800, 600);
-            this.Text = "BallGame";
-            this.BackColor = Color.MediumPurple;
+            this.BackColor = Color.Green;
+            this.Text = "BallGame:0";
+
             this.MouseClick += Program_MouseClick;
+            KeyDown += Program_KeyDown;
 
             moveTimer = new Timer();
-            moveTimer.Interval= 15;  //タイマーのインターバル(ms)
-            moveTimer.Tick += MoveTimer_Tick;//デリゲート登録
+            moveTimer.Interval = 10; //タイマーのインターバル（ms）
+            moveTimer.Tick += MoveTimer_Tick;  //デリゲート登録
+             
+        }
+
+        private void Program_KeyDown(object sender, KeyEventArgs e) {
+
         }
 
         //マウスクリック時のイベントハンドラ
         private void Program_MouseClick(object sender, MouseEventArgs e) {
             //ボールインスタンス生成
-           soccerBall = new SoccerBall(e.X -25,e.Y -25);
-            pb = new PictureBox();
-            pb.Image = soccerBall.Image;
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);  //画像の位置
-            pb.Size = new Size(50, 50); //画像の表示サイズ
+            Obj ballobj = null;
+            pb = new PictureBox();   //画像を表示するコントロール
+
+            if (e.Button == MouseButtons.Left){
+                ballobj= new SoccerBall(e.X - 25, e.Y - 25);
+                pb.Size = new Size(50, 50); //画像の表示サイズ
+            }else if(e.Button == MouseButtons.Right){
+                ballobj = new TennisBall(e.X - 12, e.Y - 12);
+                pb.Size = new Size(30, 35); //画像の表示サイズ
+            }
+
+            pb.Image = ballobj.Image;
+            pb.Location = new Point((int)ballobj.PosX, (int)ballobj.PosY); //画像の位置
             pb.SizeMode = PictureBoxSizeMode.StretchImage;  //画像の表示モード
             pb.Parent = this;
 
-            balls.Add(soccerBall);
+            balls.Add(ballobj);
             pbs.Add(pb);
+            this.Text = "BallGame:" + balls.Count;
+            moveTimer.Start();  //タイマースタート
 
-            moveTimer.Start(); //タイマースタート
         }
-        
+
         //タイマータイムアウト時のイベントハンドラ
         private void MoveTimer_Tick(object sender, EventArgs e) {
-            // soccerBall.Move();//移動
-            for (int i = 0; i < balls.Count; i++)
-            {
-                balls[i].Move();
+            for (int i = 0; i < balls.Count; i++){
+                balls[i].Move();  //移動
                 pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY); //画像の位置
             }
         }
--- Sample/BallApp/TennisBall.cs
+++ Sample/BallApp/TennisBall.cs
@@ -1,33 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BallApp {
-    class SoccerBall : Obj {
-
+    class TennisBall : Obj {
         //フィールド
         Random random = new Random();   //乱数インスタンス
 
-        //プロパティ
-
         //コンストラクタ
-        public SoccerBall(double xp, double yp)
-            :base(xp,yp, @"pic\soccer_ball.png") {
+        public TennisBall(double xp, double yp)
+            : base(xp, yp, @"pic\tennis_ball.png") {
 
             int rndX = random.Next(-25, 25);
             MoveX = (rndX != 0 ? rndX : 1); //乱数で移動量を設定
 
             int rndY = random.Next(-25, 25);
             MoveY = (rndY != 0 ? rndY : 1); //乱数で移動量を設定
-
         }
 
         //メソッド
         public override void Move() {
-            Console.WriteLine("Ｘ座標 = {0}, Ｙ座標 = {1}", PosX, PosY);
+           // Console.WriteLine("Ｘ座標 = {0}, Ｙ座標 = {1}", PosX, PosY);
             if (PosY > 520 || PosY < 0){
                 MoveY = -MoveY;
             }

77f00e3f62067d5886e59d40f19f96e205ffa5bf
櫻井彩乃 <OIBC34002@ojs.ac.jp>
2023-05-23 11:12:21 +0900

    p90まで

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -8,8 +8,21 @@ namespace Section03 {
     class Program {
         static void Main(string[] args) {
             var list = new List<string> {
-               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+               "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+            //var exists = list.Exists ( s => s[0] == 'B' );
+            //Console.WriteLine (exists);
+
+            //var name = list.Find ( s => s.Length == 5 );
+            //Console.WriteLine (name);
+
+            //var index = list.FindIndex ( s => s == "London" );
+            //Console.WriteLine ( index );
+
+            var names = list.ConvertAll ( s => s.ToLower() );
+            names.ForEach ( s => Console.WriteLine(s) );
+        
         }
     }
 }

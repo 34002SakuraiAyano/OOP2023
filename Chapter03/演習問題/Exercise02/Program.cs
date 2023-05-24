5d84d46f4fa94f88ce2b76a368557f820d223eaf
櫻井彩乃 <OIBC34002@ojs.ac.jp>
2023-05-24 11:17:08 +0900

    演習問題2-1

---
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+            Exercise2_1 ( names );
+            Console.WriteLine ();
+
+            Exercise2_2 ( names );
+            Console.WriteLine ();
+
+            Exercise2_3 ( names );
+            Console.WriteLine ();
+
+            Exercise2_4 ( names );
+        }
+
+        private static void Exercise2_1(List<string> names) {
+            Console.WriteLine ( "都市名を入力。空行で終了" );
+            var line = Console.ReadLine ();
+            var index = names.FindIndex ( s => s == line );
+            Console.WriteLine ( index );
+
+            do {
+                line = Console.ReadLine ();
+                if (string.IsNullOrEmpty ( line ))
+                    break;
+                else {
+                    index = names.FindIndex ( s => s == line );
+                    Console.WriteLine ( index );
+                }
+            } while (true);
+        }
+
+        private static void Exercise2_2(List<string> names) {
+
+        }
+
+        private static void Exercise2_3(List<string> names) {
+
+        }
+
+        private static void Exercise2_4(List<string> names) {
+
+        }
+    }
+}

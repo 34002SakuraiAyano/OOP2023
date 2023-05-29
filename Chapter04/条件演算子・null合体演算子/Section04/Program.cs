207b61a8c29f8d49c42da4d2300d385ac2c48eb2
櫻井彩乃 <OIBC34002@ojs.ac.jp>
2023-05-29 12:09:32 +0900

    TryParse

--- Chapter04/条件演算子・null合体演算子/Section04/Program.cs
+++ Chapter04/条件演算子・null合体演算子/Section04/Program.cs
@@ -8,13 +8,53 @@ namespace Section04 {
     class Program {
         static void Main(string[] args) {
 
-          var list = new List<int> { 10, 20, 30, 40, };
-            var key = 10;
+            #region 条件演算子
+            /* var list = new List<int> { 10, 20, 30, 40, };
+             var key = 10;
 
-            var num = list.Contains ( key ) ? 1 : 0; //条件演算子・三項演算子
-            Console.WriteLine ( num );
+             var num = list.Contains ( key ) ? 1 : 0; //条件演算子・三項演算子
+             Console.WriteLine ( num );*/
+            #endregion
 
+            #region null合体演算
+            /* string code = "12345";
+             var message = GetMessage ( code ) ?? DofaultMessage ();
+             Console.WriteLine ( message );*/
+            #endregion
 
+            #region
+           // Sale sale = new Sale {
+             //   Amount = 100;
+            //};
+
+            Sale sale = null;            
+
+            //「int?」はnull許容型。「?」はnull条件演算子
+            int? ret = sale?.Amount;
+
+            Console.WriteLine ( ret );
+            #endregion
         }
+
+        private static object DofaultMessage() {
+            return null;
+        }
+        private static object GetMessage(string code) {
+            return "DofaultMessage";
+        }
+    }
+
+
+    //売り上げクラス
+    public class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+
+        //商品カテゴリー
+        public string ProductCategory { get; set; }
+
+        //売上高
+        public int Amount { get; set; }
+
     }
 }

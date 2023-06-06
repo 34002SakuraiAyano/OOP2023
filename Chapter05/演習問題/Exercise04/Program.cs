3ed5aa25376883b98805124b9d1375e53fd02011
櫻井彩乃 <OIBC34002@ojs.ac.jp>
2023-06-06 14:42:04 +0900

    Exercise04 5-4

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿#define Nonarray
+#define StringArray
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +10,46 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch ();
+            sw.Start ();
+
+#if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            foreach (var words in line.Split ( ';' )) {
+                var words2 = words.Split ( '=' );
+                Console.WriteLine ( "{0}：{1}", ToJapanese ( words2[0] ), words2[1] );
+            }
+
+#elif StringArray
+            string[] lines = new string[] {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=cccc;BestWork=ぼっちゃん;Born=1990",
+                "Novelist=eeeeeee;BestWork=走れメロス;Born=2000",
+
+            };
+            foreach (var line in lines) {
+                foreach (var words in line.Split ( ';' )) {
+                    var words2 = words.Split ( '=' );
+                    Console.WriteLine ( "{0}：{1}", ToJapanese ( words2[0] ), words2[1] );
+                }
+            }
+
+#endif
+            Console.WriteLine ( "実行時間 = {0}", sw.Elapsed.ToString ( @"ss\.fffff" ) );
+        }
+
+        static string ToJapanese(string key) {
+            switch (key) {
+                case "Novelist":
+                    return "作家";
+
+                case "BestWork":
+                    return "代表作";
+
+                case "Born":
+                    return "誕生年";
+            }
+            throw new ArgumentException ( "正しい引数ではない" );
         }
     }
 }
---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> {9,7,5,3,4,5,8,7,8,5,9, };
+            Console.WriteLine (numbers.Average());
+
+            var books = Books.GetBooks ();
+
+            var sortbooks = books.Where( x=> x.Title.Contains( "物語" )).OrderByDescending(x=> x.Price) ;
+            foreach (var book in sortbooks) {
+                Console.WriteLine ( "{0}:{1}円",book.Title ,book.Price);
+            }
+        }
+    }
+}

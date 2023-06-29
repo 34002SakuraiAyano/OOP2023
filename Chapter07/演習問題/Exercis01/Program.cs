73ac28c1ad2c26128826e2b11b2bb4d6966a9046
櫻井彩乃 <OIBC34002@ojs.ac.jp>
2023-06-29 11:44:13 +0900

    Exercise1_1

---
+++ Chapter07/演習問題/Exercis01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercis01 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen".ToUpper ();
+
+            Exercise1_1 ( text );
+            Console.WriteLine ();
+            //Exercise1_2 ( text );
+
+        }
+
+        private static void Exercise1_1(string text) {
+            var dict = new Dictionary<Char, int> ();
+            int count = 0;
+            foreach (var c in text) {
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey ( c )) {
+                        dict[c]++;     //既に存在する文字ADD
+                    }
+                    else {
+                        dict[c] = 1;  //未登録（新規格納）
+                    }
+                }
+                else {
+                    //空白の処理
+                }
+            }
+            foreach (var item in dict.OrderBy( s => s.Key )) {
+                Console.WriteLine ("{0}：{1}" , item.Key,item.Value);
+            }
+        }
+    }
+}
+       // private static void Exercise1_2(string text) {
+
+       // }
+

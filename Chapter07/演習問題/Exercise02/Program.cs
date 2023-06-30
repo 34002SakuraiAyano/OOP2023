bf1841bb353e4c018a4d1543463b0ebe83bdbc6a
櫻井彩乃 <OIBC34002@ojs.ac.jp>
2023-06-30 09:59:12 +0900

    演習問題7-2-2

--- Chapter07/演習問題/Exercise02/Program.cs
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            //コンストラクタ呼び出し
+            var abbrs = new Abbreviations ();
+
+            //ADD呼び出し
+            abbrs.Add ( "IOC", "国際オリンピック委員会" );
+            abbrs.Add ( "NPT", "核兵器不拡散条約" );
+
+            //7-2-3
+            //上のADDメソッドで、2つのオブジェクト
+
+            int count = abbrs.Count;
 
         }
     }

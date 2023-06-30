87b72881f46fdda63b76953939db321957e910c6
櫻井彩乃 <OIBC34002@ojs.ac.jp>
2023-06-30 11:40:24 +0900

    演習問題8-1

---
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            foreach (var dayofWeek in Enum.GetValues(typeof(DayOfWeek))) {
+
+            }
+
+        }
+    }
+}

2b1f2ac80f3a0ab3980dbdd3f68b1696938260bb
彩乃 櫻井 <OIBC34002@ojs.ac.jp>
2023-10-10 09:56:26 +0900

    Exercise04

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise04 {
@@ -10,10 +11,12 @@ namespace Exercise04 {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
 
+            var newlines = lines
+                .Select ( s => Regex.Replace( s, @"\b(v|V)ersion\s*=\s*""v4.0""", @"version=""v5.0""" ) );
+            //　\s* 任意の数の空白文字
 
-
-
-
+            //書き込み
+            File.ReadAllLines ( "sample.txt" );
 
             // これ以降は確認用
             var text = File.ReadAllText("sample.txt");

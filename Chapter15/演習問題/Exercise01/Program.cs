42c3786915bd937a1701861a61df79eff02aca11
彩乃 櫻井 <OIBC34002@ojs.ac.jp>
2023-10-12 10:40:21 +0900

    演習問題Exercise03(2)

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -13,19 +13,19 @@ namespace Exercise01 {
 
             Exercise1_3 ();
             Console.WriteLine ();
-            
+
             //Exercise1_4 ();
             //Console.WriteLine ();
-            
+
             //Exercise1_5 ();
             //Console.WriteLine ();
-            
+
             //Exercise1_6 ();
             //Console.WriteLine ();
-            
+
             //Exercise1_7 ();
             //Console.WriteLine ();
-            
+
             //Exercise1_8 ();
 
             Console.ReadLine ();
@@ -42,27 +42,47 @@ namespace Exercise01 {
                                 .GroupBy ( b => b.PublishedYear )
                                 .OrderBy ( g => g.Key );
             foreach (var g in groups) {
-                Console.Write ( $"{g.Key}年 : " );
-                Console.WriteLine ( g.Count() );
+                Console.WriteLine ( "{0}年 : {1}冊 " ,g.Key  ,g.Count() );
             }
+
+            //var groups = Library.Books.GroupBy ( b => b.PublishedYear )
+            //        .Select(s => new { PublishedYear = s.Key, Count = s.Count () } )
+            //        .OrderBy ( g => g.PublishedYear );
+            //foreach (var item in groups) {
+            //    Console.WriteLine ( "{0}年 : {1}冊 " , item.PublishedYear , item.Count );
+            //}
         }
 
         private static void Exercise1_4() {
+            //var selected = Library.Books.OrderByDescending( b => b.PublishedYear)
+            //    .ThenByDescending(b => b.Price)
+            //    .Join(Library.Categories,
+            //        book => book.CategoryId,
+            //        category => category.Id,
+            //        (book,category ) => new {
+            //            Title = book.Title,
+            //            Category = category.Name,
+            //            PublishedYear = book.PublishedYear
+            //        });
+            //foreach (var book in selected) {
+            //    Console.WriteLine ( $"{book.PublishedYear}年 {book.} {book.Title}({category.n})" );
+            //}
         }
 
         private static void Exercise1_5() {
+        
         }
 
         private static void Exercise1_6() {
-            throw new NotImplementedException ();
+        
         }
 
         private static void Exercise1_7() {
-            throw new NotImplementedException ();
+        
         }
 
         private static void Exercise1_8() {
-            throw new NotImplementedException ();
+        
         }
     }
 }

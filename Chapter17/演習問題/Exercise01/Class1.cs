594d32fa7a439659055bc65458e8f3f99425a8fe
彩乃 櫻井 <OIBC34002@ojs.ac.jp>
2023-10-24 11:55:17 +0900

    WPF(サンプル)

---
+++ Chapter17/演習問題/Exercise01/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Exercise01 {
+    // List 17-8
+    public abstract class TextProcessor {
+
+        public static void Run<T>(string fileName) where T : TextProcessor, new() {
+            var self = new T ();
+            self.Process ( fileName );
+        }
+
+        private void Process(string fileName) {
+            using (var sr = new StreamReader ( fileName )) {
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine ();  //1行読み取る
+                    Execute ( line );
+                }
+            }
+        }
+
+        protected virtual void Execute(string line) { }
+    }
+}
---
+++ Chapter17/演習問題/Exercise03/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Exercise03 {
+    // List 17-8
+    public class TextProcessor {
+        private ITextFileService _service;
+
+        public TextProcessor (ITextFileService service) {
+            _service = service;
+        }
+
+        private void Run(string fileName) {
+            _service.Initialaize ( fileName );
+            using (var sr = new StreamReader ( fileName )) {
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine ();  //1行読み取る
+                    _service.Execute ( line );
+                }
+            }
+            _service.Terminate ();
+        }
+    }
+}
